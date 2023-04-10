5ba37e490188212435cf4a8efe5e952fd5f528ac
beyzagoknl <beyzagoknl@gmail.com>
2023-04-10 13:38:38 +0200

    added profile photo information server

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import userRouter from "./routes/user.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 app.use(express.json());
